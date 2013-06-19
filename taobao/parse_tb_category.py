16002f09f615c6b13f1f1bcee5f5853954928d95
richardxu <iamacnhero@gmail.com>
2013-06-19 10:11:46 +0800

    tb

--- taobao/parse_tb_category.py
+++ taobao/parse_tb_category.py
@@ -3,7 +3,7 @@
 # author: richardxu
 # time: 2013-06-17
 # purpose: 解析淘宝类目页面, 得到近一月的销量
-# usage: python parse_tb_category.py $category_id $pagenum, eg: 
+# usage: python parse_tb_category.py $category_id $pagenum, eg: python parse_tb_category.py 50019172 5
 
 import os
 import sys
