7e622953d7be3761ed8c2706cd0e772179a7d158
richardxu <iamacnhero@gmail.com>
2013-06-30 19:01:25 +0800

    MapReduce best practices

--- mapreduce/java/src/com/xufeng/hdfs/ReadConfig.java
+++ mapreduce/java/src/com/xufeng/hdfs/ReadConfig.java
@@ -1,14 +1,15 @@
 package com.xufeng.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
+import java.util.Map.Entry;
 
 public class ReadConfig {
 	public static void main(String[] args) {
 		Configuration conf = new Configuration();
 		conf.addResource("cor-site.xml");
 		conf.addResource("hdfs-site.xml");
-		System.out.println(conf.get("fs.default.name"));
-		System.out.println(conf.get("dfs.replication"));
-		System.out.println(conf.getInt("dfs.replication", 0));	
+		for (Entry<String, String> entry: conf) {
+			System.out.printf("%s = %s\n", entry.getKey(), entry.getValue());
+		}
 	}
 }
