157eb6255d9ea8f78277608a8afa0ef33e60903d
richardxu <iamacnhero@gmail.com>
2014-07-04 23:35:30 +0800

    update

--- spring/spring-redis-skeleton/src/test/java/com/richardxu/service/RedisTest.java
+++ spring/spring-redis-skeleton/src/test/java/com/richardxu/service/RedisTest.java
@@ -4,28 +4,29 @@ import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 
 public class RedisTest extends TestBase {
 	@Autowired
 	private TestService ts;
 
 	@Autowired
-	private RedisService<String> redisService;
+	private RedisTemplate<String, String> redisTemplate;
 	
 	@Test
 	public void testSet() {
-		redisService.getValueOpeartions().set("nations", "china,usa,japan,korea,russia");
-		redisService.getListOperations().leftPush("nationList", "china");
-		redisService.getListOperations().leftPush("nationList", "usa");
-		redisService.getListOperations().leftPush("nationList", "japan");
+		redisTemplate.opsForValue().set("nations", "china,usa,japan,korea,russia");
+		redisTemplate.opsForList().leftPush("nationList", "china");
+		redisTemplate.opsForList().leftPush("nationList", "usa");
+		redisTemplate.opsForList().leftPush("nationList", "japan");
 	}
 	
 	@Test
 	public void testGet() {
-		String nations = redisService.getValueOpeartions().get("nations");
+		String nations = redisTemplate.opsForValue().get("nations");
 		System.out.println(nations);
 
-		List<String> nationList = redisService.getListOperations().range("nationList", 0, -1);
+		List<String> nationList = redisTemplate.opsForList().range("nationList", 0, -1);
 		System.out.println(nationList);
 	}
 	
