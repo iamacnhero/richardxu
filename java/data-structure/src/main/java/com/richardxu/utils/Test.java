d0f1970597bba77ed4abf1efee76229f0e0f91e1
richardxu <iamacnhero@gmail.com>
2014-07-14 00:44:48 +0800

    update

--- java/data-structure/src/main/java/com/richardxu/utils/Test.java
+++ java/data-structure/src/main/java/com/richardxu/utils/Test.java
@@ -1,32 +1,17 @@
 package com.richardxu.utils;
 
-import com.richardxu.datastructure.Bag;
-import com.richardxu.lib.StdIn;
-import com.richardxu.lib.StdOut;
+import com.richardxu.datastructure.Stack;
 
 public class Test {
     
     public static void main(String[] args) {
-        Bag<Double> numbers = new Bag<Double>();
-        while (!StdIn.isEmpty()) {
-            double item = StdIn.readDouble();
-            if (((int) item) == 0000)
-                break;
-            numbers.add(StdIn.readDouble());
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(3);
+        stack.push(5);
+        stack.push(6);
+        for (Integer i : stack) {
+            System.out.println(i);
         }
-        int len = numbers.size();
-        double sum = 0.0;
-        for (Double d : numbers) {
-            sum += d;
-        }
-        double mean = sum / len;
-        System.out.printf("Mean: %.2f\n", mean);
         
-        sum = 0.0;
-        for (Double d : numbers) {
-            sum += (d - mean) * (d - mean);
-        }
-        double std = Math.sqrt(sum / (len - 1));
-        System.out.printf("Std dev: %.2f", std);
     }
 }
