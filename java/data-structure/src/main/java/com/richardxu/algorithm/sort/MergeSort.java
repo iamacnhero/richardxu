916aa6a19bd1c3df1f9d3075fda2b7398604e144
richardxu <iamacnhero@gmail.com>
2014-07-29 01:30:17 +0800

    update

--- java/data-structure/src/main/java/com/richardxu/algorithm/sort/MergeSort.java
+++ java/data-structure/src/main/java/com/richardxu/algorithm/sort/MergeSort.java
@@ -6,40 +6,41 @@ package com.richardxu.algorithm.sort;
  * @date 2014-7-28 下午7:32:53
  *
  */
+@SuppressWarnings({ "rawtypes", "unchecked" })
 public class MergeSort<T extends Comparable<T>> {
     private MergeSort() {}
     
-    // stably merge a[lo .. mid] with a[mid+1 .. hi] using aux[lo .. hi]
-    private static <T extends Comparable<T>> void merge(T[] a, T[] aux, int lo, int mid, int hi) {
-        // copy to aux[]
+    private static Comparable[] aux; // 归并所需的辅助数组
+    
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];     // 一次性分配空间
+        sort(a, 0, a.length - 1);
+    }
+    
+    private static <T extends Comparable<T>> void sort(T[] a, int lo, int hi) {
+        // 将数组a[lo..hi]排序
+        if (hi <= lo) return ;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);       // 将左半边排序
+        sort(a, mid+1, hi);     // 将右半边排序
+        merge(a, lo, mid, hi);  // 归并结果
+    }
+    
+    // 将a[lo..mid]和a[mid+1..hi]归并
+    private static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo, j = mid + 1;
+        
         for (int k = lo; k <= hi; k++) {
-            aux[k] = a[k];
+            aux[k] = a[k]; 
         }
         
-        // merge back to a[]
-        int i = lo, j = mid + 1;
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid)                    a[k] = aux[j++];
-            else if (j > hi)                a[k] = aux[i++];
-            else if (less(aux[i], aux[j]))  a[k] = aux[j++];
-            else                            a[k] = aux[i++];
+        for (int k = lo; k <= hi; k++) {    // 归并到a[lo..hi]
+            if      (i > mid)              a[k] = aux[j++]; // 左半边用尽(取右半边的元素)
+            else if (j > hi)               a[k] = aux[i++]; // 右半边用尽(取左半边的元素)
+            else if (less(aux[i], aux[j])) a[k] = aux[j++]; // 左半边的当前元素大于右半边的当前元素(取右半边的元素)
+            else                           a[k] = aux[i++]; // 右半边的当前元素大于等于左半边的当前元素(取左半边的元素)
         }
-    }
-    
-    // mergesort a[lo..hi] using auxiliary array aux[lo..hi]
-    private static <T extends Comparable<T>> void sort(T[] a, T[] aux, int lo, int hi) {
-        if (lo >= hi) return ;
-        int mid = lo + (hi - lo) / 2;
-        sort(a, aux, lo, mid);
-        sort(a, aux, mid + 1, hi);
-        merge(a, aux, lo, mid, hi);
-    }
-    
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    public static void sort(Comparable[] a) {
-        Comparable[] aux = new Comparable[a.length];
-        sort(a, aux, 0, a.length-1);
-    }
+    }    
     
     private static <T extends Comparable<T>> boolean less(T v, T w) {
         return (v.compareTo(w) > 0);
