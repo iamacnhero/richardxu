b1d83c67454c46606de0d55d443d52ed07711a0b
richardxu <iamacnhero@gmail.com>
2014-08-12 12:36:08 +0800

    update

--- java/libs/toolkit-common-lang/src/main/java/com/richardxu/common/lang/EnumUtil.java
+++ java/libs/toolkit-common-lang/src/main/java/com/richardxu/common/lang/EnumUtil.java
@@ -1,25 +1,36 @@
-/**
- * 
- */
 package com.richardxu.common.lang;
 
+import java.util.Random;
+
 /**
- * @author <a href="mailto:xuc@iphonele.com">saga67</a>
- * 
- * @version create on Nov 12, 2013 5:13:44 PM
+ * @author <a href="mailto:richardxu@live.cn">xufeng</a>
+ * @date 2014-8-12 下午12:27:45
+ *
  */
 public final class EnumUtil {
+    private static Random rand = new Random();
 
-	private EnumUtil() {
+    private EnumUtil() {
 
-	}
+    }
 
-	public static <T extends Enum<T>, F extends Enum<F>> Enum<T> mapping(
-			Class<T> type, Enum<F> from) {
-		String name = from.name();
-
-		return Enum.valueOf(type, name);
-
-	}
-
+    public static <T extends Enum<T>, F extends Enum<F>> Enum<T> mapping(
+            Class<T> type, Enum<F> from) {
+        String name = from.name();
+        return Enum.valueOf(type, name);
+    }
+    
+    public static <T> T random(T[] values) {
+        return values[rand.nextInt(values.length)];
+    }
+    
+    /**
+     * 随机返回一个枚举类里的元素
+     * @param ec
+     * @return
+     */
+    public static <T extends Enum<T>> T random(Class<T> ec) {
+        return random(ec.getEnumConstants());
+    }
+    
 }
