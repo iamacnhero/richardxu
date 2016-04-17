59e0826ba5d89c576d5ba43006f41b0f29c0c316
serious_xu <iamacnhero@gmail.com>
2016-04-17 23:41:24 +0800

    add factory

--- java/java-best-practice/src/main/java/com/richardxu/gof/factory/example/ExportDB.java
+++ java/java-best-practice/src/main/java/com/richardxu/gof/factory/example/ExportDB.java
@@ -4,7 +4,7 @@ public class ExportDB implements ExportFileApi {
 
 	@Override
 	public boolean export(String data) {
-		System.out.println("导出数据" + data + "到数据库备份文件");
+		System.out.println("导出数据 {" + data + "} 到数据库备份文件");
 		return false;
 	}
 
